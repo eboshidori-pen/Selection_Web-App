526e7cc59914598af8ec94353a954d5bb6502fc0
eboshidori-pen <64635773+eboshidori-pen@users.noreply.github.com>
2023-04-21 13:06:47 +0900

    Create index0.php

---
+++ index0.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>タイトル画面</title>
+
+    <!-- CSSファイルの読み込み -->
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div id="home" class="big-bg">
+
+    <!-- ページヘッダー -->
+    <header class="page-header wrapper">
+      <h1><a href="index0.php"><img class="logo" src="hotpepper-s.gif" alt="ホットペッパーWebサービスクレジット"></a></h1>
+    </header>
+
+    <!-- ホームコンテンツ -->
+    <div class="home-content wrapper">
+      <h2 class="catch-copy">あなたにぴったりのレストランを探す</h2>
+        <a class="button" href="index1.php">検索</a>
+    </div>
+
+    </div>
+  </body>
+</html>
